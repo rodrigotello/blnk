887a76bf928d54ebbf6716521453c073803e43e4
rodrigotello <rodrigotello@outlook.com>
2015-05-28 18:49:20 -0500

    Font size and line height added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
 
-  get 'static_pages/about'
+  get 'about' => 'static_pages#about'
 
-  get 'static_pages/help'
+  get 'help' => 'static_pages#help'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
